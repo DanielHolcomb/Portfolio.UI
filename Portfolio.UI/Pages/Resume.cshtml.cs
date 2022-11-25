f2c1bbee5d66d6333061f7a53d23cfa9ca3ff786
Dan <me@danielholcomb.com>
2022-11-25 02:44:06 -0700

    Extract skills to model list

--- Portfolio.UI/Pages/Resume.cshtml.cs
+++ Portfolio.UI/Pages/Resume.cshtml.cs
@@ -5,8 +5,14 @@ namespace Portfolio.UI.Pages
 {
     public class ResumeModel : PageModel
     {
+        public List<string> Skills { get; set; }
+
         public void OnGet()
         {
+            Skills = new List<string>()
+            {
+                "C#", "dotNet", "Unity", "Azure", "Git", "SQL", "SOQL", "API", "Microservice", "..."
+            };
         }
     }
 }
